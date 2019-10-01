fa1a56ebdd8f2eddbde3de5e090dbb07c305be52
Allan klar <allanklar@hotmail.com>
2019-10-01 09:38:07 -0500

    checkpoint-to-do-scripting: completed

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-     const newToDoText = document.getElementById('newToDoText');
-     const toDoList = document.getElementById('toDoList');
-      addToDoForm.addEventListener('submit', event => {
-        event.preventDefault();
+  const toDos = [];
 
-    // get the text
-    let title = newToDoText.value;
+   const addToDoForm = document.getElementById('addToDoForm');
 
-    // create a new li
-    let newLi = document.createElement('li');
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
 
-    // create a new input
-    let checkbox = document.createElement('input');
+    if (!newToDoText.value) { return; }
 
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
-    // set the title
-   newLi.textContent = title;
-
-   // attach the checkbox to the li
-   newLi.appendChild(checkbox);
-
-   // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //empty the input
     newToDoText.value = '';
 
-      });
+    renderTheUI();
+
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+
+    });
+
+  }
+
+  addToDoForm.addEventListener('submit', event => {
+      event.preventDefault();
+      createNewToDo();
+    });
+
+renderTheUI();
+
  }
 
 window.onload = function() {
